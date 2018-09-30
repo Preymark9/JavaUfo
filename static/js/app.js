12f541f6d035eb1f6a5c71ead4da8fa2e49dc056
major <majora1930@gmail.com>
2018-09-30 03:03:19 -0500

    push

--- static/js/app.js
+++ static/js/app.js
@@ -4,22 +4,7 @@ var tableData = data;
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
 
-// data.forEach((data) => {
-    //var row = tbody.append("tr");
-
-var submit = d3.select("#filter-btn");
-
 function BuildTable(tableData) {
-    //d3.event.preventDefault();
-    
-    // var datetime = data.values(datetime);
-    // var city = data.values(city);
-    // var state = data.values(state);
-    // var country = data.values(country);
-    // var shape = data.values(shape);
-    // var duration = data.values(duration);
-    // var comments = data.values(comments);
-    
     tableData.forEach(Value => {
        var row = tbody.append("tr");
        Object.entries(Value).forEach(([key, value]) => {
@@ -28,24 +13,39 @@ function BuildTable(tableData) {
   });
   });
   }
+//What I did above was make a function to load the table into the webpage. This was a bit more difficult than I expected. Rob helped a lot. 
+//next goal is to make a successful filter. 
+var submit = d3.select("#filter-btn");
 
-//submit.on("click", function(BuildTable) {
-//   d3.event.preventDefault();
+submit.on("click", function(filter) {
+   d3.event.preventDefault();
   
-//   var datetime = data.values(datetime);
-//   var city = data.values(city);
-//   var state = data.values(state);
-//   var country = data.values(country);
-//   var shape = data.values(shape);
-//   var duration = data.values(duration);
-//   var comments = data.values(comments);
-  
-//   data.forEach((tableData) => {
-//      var row = tbody.append("tr");
-//      Object.entries(tableData).forEach(([key, value]) => {
-//          var cell = tbody.append("td");
-//          cell.text(value);
-// });
-// });
-// })
+   var inputElement = d3.select("#datetime");
+
+   var inputValue = inputElement.property("value");
+
+   var filteredData = tableData.filter(date => date.datetime === inputValue);
+
+   var mapper = filteredData.map(date => date.datetime);
+   //I forgot to map my filter. Problem is now I have to redefine my variables a little
+   
+   var datetime = data.values(datetime);
+   var city = data.values(city);
+   var state = data.values(state);
+   var country = data.values(country);
+   var shape = data.values(shape);
+   var duration = data.values(duration);
+   var comments = data.values(comments);
+     
+   d3.select(".table table-striped")
+    .append("li").text(`datetime: ${datetime}`)
+    .append("li").text(`city: ${city}`)
+    .append("li").text(`state: ${state}`)
+    .append("li").text(`country: ${country}`)
+    .append("li").text(`shape: ${shape}`)
+    .append("li").text(`duration:${duration}`)
+    .append("li").text(`comments:${comments}`);
+});
+//I'm pretty sure I built the function for filtering right, but it isn't working in practice, not sure why. 
+//theory is that it's the var filteredData
 BuildTable(tableData);
